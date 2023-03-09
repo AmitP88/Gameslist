4e743b15869a024c69f03c1730636ef7e73a8995
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2023-03-09 15:34:48 -0800

    rendered 'Hello Mobile' and 'Hello Desktop' texts using Material UI breakpoints when resizing the browser

--- components/App.tsx
+++ components/App.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+import { useMediaQuery, useTheme } from "@mui/material";
+
 export default function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.only("xs")); // between 0 and 600px
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm")); // above 900px
+
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  )
+    <React.Fragment>
+      {isMobile && <span>Hello Mobile</span>}
+      {isDesktop && <span>Hello Desktop</span>}
+    </React.Fragment>
+  );
 }
