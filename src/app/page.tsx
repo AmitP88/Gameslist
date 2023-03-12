2a4ea835ce9c08dfb4e6b9b9b252c3d64192ce9e
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2023-03-12 13:53:05 -0700

    added a toggle dark mode button above heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,61 @@
 
 import App from '@/components/App';
 
-export default function Home() {
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function ToggleDark() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
-    <div className="Home">
-      <App />
-    </div>
-  )
+    <Box
+      sx={{
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        p: 1,
+        height: '100%'
+      }}
+    >
+      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+      <App />      
+    </Box>
+  );
+}
+
+export default function Home() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <ToggleDark />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
