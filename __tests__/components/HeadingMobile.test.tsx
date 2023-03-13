75af9e2562e7935c8f0c22f1c9b8dbb3d3fb54fa
Amit Patel <amitpatel@Amits-MacBook-Pro.local>
2023-03-13 12:22:32 -0700

    removed import statement of ListButtons component

--- __tests__/components/HeadingMobile.test.tsx
+++ __tests__/components/HeadingMobile.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Logo from "../../components/Logo";
-import ListButtons from "../../components/ListButtons";
 
 describe("HeadingMobile component", () => {
   it("renders Logo", () => {
